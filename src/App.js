0aafe8421abede9bd0b93389f32bc68166712cb4
Oplala <oplala2020@gmail.com>
2023-08-29 18:56:04 +0530

    client

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/:projectId" element={<Task />} />
         <Route path="/" element={
           <div className="flex flex-col items-center w-full pt-10">
-            <img src="./image/welcome.svg" className="w-5/12" alt="" />
+            <img src="./images/welcome.svg" className="w-5/12" alt="" />
             <h1 className="text-lg text-gray-600">Select or create new project</h1>
           </div>
         } />
